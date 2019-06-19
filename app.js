e08a28bd87eef9a7a5c8ec118d997c9b4058786c
Sviatoslav <newmedia27@gmail.com>
2019-06-19 12:33:00 +0300

    added user win

--- app.js
+++ app.js
@@ -11,12 +11,10 @@ GAME RULES:
 
 const RESET_VALUE = 2;
 
-let scores = [0, 0];
 let activePlayer = 0;
 let current = 0;
 const LIMIT = 100;
 let playerNumber = 0;
-const winner = [];
 const diceElement = document.querySelectorAll('.dice');
 
 const limit = document.querySelector('.limit__content');
@@ -41,19 +39,43 @@ Gamer.prototype.resetScore = function () {
 }
 
 
-const players = ['player1', 'player2'].map(e => {
-        const result = prompt(e + 'you name', '')
-        if (result) { //TODO
-            return new Gamer(result, 0, 0)
-        } else {
-            return new Gamer(`Player ` + ++playerNumber, 0, 0)
-        }
+const getWinnerStore = () => {
+
+    if (localStorage.getItem('winner')) {
+        return JSON.parse(localStorage.getItem('winner'));
+    } else {
+        return [];
     }
-)
+}
 
 
-console.log(players);
+let players = [];
 
+
+const initPlayer = (arr) => {
+
+    return arr.reduce((array, e, i) => {
+        let result = prompt(e + 'you name', '')
+        if (array && array.some(item => item.name === result)) {
+            return [...array, new Gamer(`Player ` + ++playerNumber, 0, 0)]
+        }
+        if (result && validatePlayer(result)) {
+            return [...array, new Gamer(result, 0, 0)]
+        } else if (!result) {
+            return [...array, new Gamer(`Player ` + ++playerNumber, 0, 0)]
+        } else {
+            if (confirm('Это точно ВЫ?')) {
+                return [...array, new Gamer(result, 0, 0)]
+            }
+        }
+
+
+    }, [])
+}
+
+const validatePlayer = (str) => {
+    return !(getWinnerStore().length && getWinnerStore().some(e => e.name === str));
+}
 
 const initGame = () => {
     document.querySelector('#current-0').textContent = 0;
@@ -61,8 +83,10 @@ const initGame = () => {
     document.querySelector('#score-0').textContent = 0;
     document.querySelector('#score-1').textContent = 0;
     diceElement.forEach(e => e.style.display = 'none');
+    players = initPlayer(['player1', 'player2']);
     players.forEach((e, i) => {
         document.querySelector(`#name-${i}`).textContent = e.name
+        e.resetScore();
     })
 
 
@@ -100,9 +124,22 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
         current += sumDice(dice);
         document.getElementById('current-' + activePlayer).textContent = current;
 
-        if (scores[activePlayer] + current >= +limit.value) {
-            winner.push(players[activePlayer].name)
+        if (players[activePlayer].getScore() + current >= +limit.value) {
+            players[activePlayer].winCount = 1;
             alert(` ${players[activePlayer].name} won!!!`);
+            const winnerArr = getWinnerStore();
+            if (winnerArr && winnerArr.length && winnerArr.some(e => e.name === players[activePlayer].name)) {
+
+                winnerArr.forEach(e => {
+                    if (e.name === players[activePlayer].name) {
+                        e.winCount += players[activePlayer].winCount
+                    }
+                })
+            } else {
+                winnerArr.push(players[activePlayer])
+            }
+
+            localStorage.setItem('winner', JSON.stringify(winnerArr));
         }
 
     } else {
@@ -140,4 +177,14 @@ document.querySelector('.btn-hold').addEventListener('click', function () {
 
 document.querySelector('.btn-new').addEventListener('click', function () {
     initGame();
+});
+document.querySelector('.btn-win').addEventListener('click', function () {
+    const winner = getWinnerStore();
+    if (winner.length) {
+        winner.sort((a, b) => b.winCount - a.winCount)
+        const list = winner.sort().map(e => (`${e.name} - ${e.winCount}\n\r`))
+        alert(list)
+    } else {
+        alert(`Тут пока пусто`)
+    }
 });
